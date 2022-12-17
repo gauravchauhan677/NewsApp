7ad08c9a3372eadee0c6a59ebed5452c4a0d5f70
Gaurav Chauhan <Gauravchauahan677@gmail.com>
2022-12-17 09:45:01 +0530

    NewsApp

---
+++ NewsApp/APICaller.swift
@@ -0,0 +1,64 @@
+//
+//  APICaller.swift
+//  NewsApp
+//
+//  Created by Gaurav Chauhan on 16/12/22.
+//
+
+import Foundation
+
+final class APICaller {
+    static let shared = APICaller()
+    
+    struct Constants {
+        static let topHeadlinesURL = URL(string: "https://newsapi.org/v2/top-headlines?country=US&apikey=4530e5824563404c8973ba6ec6b13ba8")
+    }
+    
+    private init() {}
+    
+    public func getTopStories(completion: @escaping(Result<[Article], Error>) -> Void) {
+        guard let url = Constants.topHeadlinesURL else {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, _, error in
+            if let error = error {
+                completion(.failure(error))
+            }
+            else if let data = data {
+                
+                do {
+                    let result = try JSONDecoder().decode(APIResponse.self, from: data)
+                    
+                    print("Articles: \(result.articles.count)")
+                    
+                    completion(.success(result.articles))
+                }
+                catch{
+                    completion(.failure(error))
+                }
+            }
+        }
+        task.resume()
+    }
+    
+}
+
+// Models
+
+struct APIResponse: Codable {
+    let articles: [Article]
+}
+
+struct Article: Codable {
+    let source: Source
+    let title: String
+    let description: String?
+    let url: String?
+    let urlToImage: String?
+    let publishedAt: String
+}
+
+struct Source: Codable {
+    let name: String
+}
